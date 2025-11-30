37a49158dd9a455a328da6fc8201e15481abcce2
Marko Rajcic <marko.rajcic@camcellnet.com>
2025-11-30 21:56:47 +0100

    asd

--- np/np/settings.py
+++ np/np/settings.py
@@ -113,48 +113,31 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEBUG_PROPAGATE_EXCEPTIONS = True
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
 
-    "handlers": {
-        "file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": "/home/marko/projects/neuropink/neuropink/logs/neuropink.log",
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            # Ensure this path is writable
+            'filename': '/home/marko/projects/neuropink/neuropink/logs/neuropink.log',
         },
-        "console": {
-            "class": "logging.StreamHandler",
-            "level": "ERROR",
+        'console': {
+            'class': 'logging.StreamHandler',
         },
     },
-
-    "loggers": {
-        # Main Django logger → log only errors
-        "django": {
-            "handlers": ["file", "console"],
-            "level": "ERROR",
-            "propagate": True,
+    'loggers': {
+        'django.security': {
+            'handlers': ['file', 'console'],
+            'level': 'WARNING',
+            'propagate': True,
         },
-        "django.security.DisallowedHost": {
-            "handlers": ["file", "console"],
-            "level": "ERROR",
-            "propagate": False,
-        },
-
-        # Silence SQL queries
-        "django.db.backends": {
-            "handlers": [],
-            "level": "WARNING",  # WARNING prevents SQL DEBUG logging
-            "propagate": False,
-        },
-
-        # Log only errors from requests (404, 500)
-        "django.request": {
-            "handlers": ["file", "console"],
-            "level": "ERROR",
-            "propagate": False,
+        'django.request': {
+            'handlers': ['file', 'console'],
+            'level': 'WARNING',
+            'propagate': True,
         },
     },
 }
