1ea9ea2da654ed9f4775e179700ad72d806fc6ab
Marko Rajcic <marko.rajcic@camcellnet.com>
2025-11-30 21:36:40 +0100

    asd

--- np/np/settings.py
+++ np/np/settings.py
@@ -111,48 +111,48 @@ STATIC_ROOT = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEBUG_PROPAGATE_EXCEPTIONS = True
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
+# DEBUG_PROPAGATE_EXCEPTIONS = True
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
 
-    "handlers": {
-        "file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": "/home/marko/projects/neuropink/neuropink/logs/neuropink.log",
-        },
-        "console": {
-            "class": "logging.StreamHandler",
-            "level": "ERROR",
-        },
-    },
+#     "handlers": {
+#         "file": {
+#             "level": "ERROR",
+#             "class": "logging.FileHandler",
+#             "filename": "/home/marko/projects/neuropink/neuropink/logs/neuropink.log",
+#         },
+#         "console": {
+#             "class": "logging.StreamHandler",
+#             "level": "ERROR",
+#         },
+#     },
 
-    "loggers": {
-        # Main Django logger → log only errors
-        "django": {
-            "handlers": ["file", "console"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-        "django.security.DisallowedHost": {
-            "handlers": ["file", "console"],
-            "level": "ERROR",
-            "propagate": False,
-        },
+#     "loggers": {
+#         # Main Django logger → log only errors
+#         "django": {
+#             "handlers": ["file", "console"],
+#             "level": "ERROR",
+#             "propagate": True,
+#         },
+#         "django.security.DisallowedHost": {
+#             "handlers": ["file", "console"],
+#             "level": "ERROR",
+#             "propagate": False,
+#         },
 
-        # Silence SQL queries
-        "django.db.backends": {
-            "handlers": [],
-            "level": "WARNING",  # WARNING prevents SQL DEBUG logging
-            "propagate": False,
-        },
+#         # Silence SQL queries
+#         "django.db.backends": {
+#             "handlers": [],
+#             "level": "WARNING",  # WARNING prevents SQL DEBUG logging
+#             "propagate": False,
+#         },
 
-        # Log only errors from requests (404, 500)
-        "django.request": {
-            "handlers": ["file", "console"],
-            "level": "ERROR",
-            "propagate": False,
-        },
-    },
-}
+#         # Log only errors from requests (404, 500)
+#         "django.request": {
+#             "handlers": ["file", "console"],
+#             "level": "ERROR",
+#             "propagate": False,
+#         },
+#     },
+# }
